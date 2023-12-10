cbfe73ca0ef503f8bdb9ed982c0bd72808f53cee
Sayyeda Afshan <syed1995.sa@gmail.com>
2023-12-10 20:54:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -38,5 +38,5 @@
 };
 
 // Do not the change the code below
-const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+const C = prompt("Enter c: ");
+alert(JSON.stringify(makeChange(C)));
