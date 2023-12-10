5140b310b31d2c2656a867396eaee529fa9b39b9
Sayyeda Afshan <syed1995.sa@gmail.com>
2023-12-10 20:52:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
-const makeChange = (c) => {
+
   // your name here
+	const makeChange = (c) => {
+	//created an object
+	let result  ={
+	q:0,
+	d:0,
+	n:0, 
+	p: 0,
+};
+//i need to devide that with 1,
+//5 , 10 , 25 
+	while (c>0) {
+	if(c>=25)
+		//
+		{c= c-25;
+			result.q++;}
+//23
+		else if(c>=10)
+		{
+			c=c-10;
+            //c=13
+            //c=3
+			result.d++;
+            //d=1
+            //d=2
+		}
+		else if(c>=5)
+		{
+			c=c-5;
+			result.n++; 
+
+		}
+		else{
+			c=c-1;
+			result.p++;
+		}
 };
 
 // Do not the change the code below
